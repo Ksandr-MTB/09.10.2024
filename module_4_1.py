cde5567d67aa5a71a39e9549f6867e4c2a26efaa
user <sasha.sapegin.99@gmail.com>
2024-09-26 20:15:56 +0700

    4 модуль

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as fd
+
+
+import true_math as td
+fd(5, 2)
+fd(5, 0)
+fd(5, 5)
+td.divide1(5, 2)
+td.divide1(5, 0)
+td.divide1(5, 5)
+
+
+
---
+++ true_math.py
@@ -0,0 +1,9 @@
+def divide1(first, second):
+    from math import inf
+    a = inf
+    if second == 0:
+
+        print(a)
+    else:
+        print(first / second)
+
