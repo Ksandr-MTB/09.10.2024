14e9062e3a01ac2c112b4635a312415e0f4bc1b9
user <sasha.sapegin.99@gmail.com>
2024-10-02 20:55:02 +0700

    4 модуль

--- module_4_2.py
+++ module_4_2.py
@@ -10,5 +10,7 @@ def test_function():
 
 test_function()
 
-inner_function()
+#inner_function()  Вызов данной функции выдает
+#                   ошибку "name 'inner_function' is not defined.
+#                   Did you mean: 'test_function'?"
 
