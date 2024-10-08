71811b9d112cdc4dfa9debcb21c537304120821b
user <sasha.sapegin.99@gmail.com>
2024-10-08 20:49:55 +0700

    4 модуль

--- module_5_1.py
+++ module_5_1.py
@@ -10,11 +10,28 @@ class House:
             for i in range(1, new_floor + 1):
                 print(i)
 
+    def __len__(self):
+        print(self.number_of_floors)
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
+    def __str__(self):
+        print(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
 h1.go_to(5)
 h2.go_to(10)
+h1.__str__()
+h2.__str__()
+h1.__len__()
+h2.__len__()
 
 # print(h1.name, h1.number_of_floors)
 # print(h2.name, h2.number_of_floors)
+
+# def exponentiation(n):
+#     print(n ** 2, n ** 3)
+#
+#
+# exponentiation(5)
