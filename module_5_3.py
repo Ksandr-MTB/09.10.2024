28f05bc14851bbe9f99dccc32291dbd56ec60785
user <sasha.sapegin.99@gmail.com>
2024-10-12 21:25:41 +0700

    4 модуль

--- module_5_3.py
+++ module_5_3.py
@@ -1,4 +1,12 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        # print(*args, **kwargs)
+        cls.houses_history.append(args[0])
+        # print(cls.houses_history)
+        return super().__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
@@ -9,6 +17,9 @@ class House:
         else:
             for i in range(1, new_floor + 1):
                 print(i)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
 
     def __len__(self):
         print(self.number_of_floors)
@@ -40,26 +51,36 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
 # h1.go_to(5)
 # h2.go_to(10)
-h1.__str__()
-h2.__str__()
+# h1.__str__()
+# h2.__str__()
 # h1.__len__()
 # h2.__len__()
 
 
-print(h1 == h2)
-h1.__add__(10)
-h1.__str__()
-print(h1 == h2)
-h1.__add__(10)
-h1.__str__()
-h2.__add__(10)
-h2.__str__()
+# print(h1 == h2)
+# h1.__add__(10)
+# h1.__str__()
+# print(h1 == h2)
+# h1.__add__(10)
+# h1.__str__()
+# h2.__add__(10)
+# h2.__str__()
+#
+# print(h1 > h2)
+# print(h1 >= h2)
+# print(h1 < h2)
+# print(h1 <= h2)
+# print(h1 != h2)
+# h1.__del__()
 
-print(h1 > h2)
-print(h1 >= h2)
-print(h1 < h2)
-print(h1 <= h2)
-print(h1 != h2)
+
